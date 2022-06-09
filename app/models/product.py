3c57c6a1ee7a71ff31d7d151e9050588b2acabc0
SzymonFit <s222728@student.uek.krakow.pl>
2022-06-09 23:22:15 +0200

    zadanie

--- app/models/product.py
+++ app/models/product.py
@@ -1,5 +1,6 @@
 
 class Product:
-    def __init__():
-        pass
+    def __init__(self, product_id, all_opinions):
+        self.product_id = product_id
+        self.all_opinions = all_opinions
 #konstruktory dla produktu i opinii i cos lepiej zrobic zeby ladniej wygladalo
